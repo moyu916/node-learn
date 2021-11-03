47f3bfb80e574045180644c7a4eea4d361bddfbb
moyu916 <972413091@qq.com>
2021-11-03 22:54:36 +0800

    config封装环境配置&&db封装sql_API

---
+++ sql-demo.sql
@@ -0,0 +1,33 @@
+use myblog;
+
+-- show tables; 
+
+insert into users(username, `password`, realname) values ('lisi', '123', '李四');
+insert into users(username, `password`, realname) values ('zhangsan', '456', '张三');
+insert into users(username, `password`, realname) values ('zhangergou', '456', '张二狗');
+
+select * from users where state=1;
+select * from users where state<>0;
+
+select id, username from users 
+	where username like '%zhang%'
+    order by id desc;
+    
+update users set `password`='789' where username='zhangergou';
+
+-- 软删除, 一般不会用delete直接删, 而是通过设置state
+update users set state=0 where username='zhangergou';
+
+-- -------------------------------------------
+select * from blogs;
+
+insert into blogs(title, content, createtime, author) values ('标题C', '内容C', 1635928732012, 'lisi');
+
+select * from blogs
+	where author='lisi'
+	order by createtime desc;
+
+select * from blogs 
+	where title like '%标题%'
+    order by createtime desc; 
+    
---
+++ src/db/mysql.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql')
+const { MYSQL_CONF } = require('../config/db')
+
+const con = mysql.createConnection(MYSQL_CONF)
+
+con.connect()
+
+const exec = (sql) => {
+    return new Promise((resolve, reject) => {
+        con.query(sql, (err, result) => {
+            if (err) {
+                reject(err)
+            }
+            resolve(result)
+        })
+    })
+}
+
+module.exports = {
+    exec
+}
